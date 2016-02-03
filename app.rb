4fd80f2295fe8006705576d5c07e1287511ad6cd
Vicky Gray <vickygray26@hotmail.com>
2016-02-03 12:32:33 +0000

    post, redirect, get

--- app.rb
+++ app.rb
@@ -1,6 +1,8 @@
 require 'sinatra/base'
 
+
 class Battle < Sinatra::Base
+  enable :sessions
 
   get '/' do
     erb(:index)
@@ -8,8 +10,14 @@ class Battle < Sinatra::Base
 
   post '/names' do
     p params
-    @player_1_name = params[:player_1_name]
-    @player_2_name = params[:player_2_name]
+    session[:player_1_name] = params[:player_1_name]
+    session[:player_2_name] = params[:player_2_name]
+    redirect to('/play')
+  end
+
+  get '/play' do
+    @player_1_name = session[:player_1_name]
+    @player_2_name = session[:player_2_name]
     erb(:play)
   end
 
