92c45760b5c48e4e8750534fb1896e311d5e07a6
JoeWroe <joe_wroe@icloud.com>
2016-02-03 17:22:32 +0000

    Battle: User Story 2
    	# As Player 1,
    	# So I can see how close I am to winning
    	# I want to see Player 2's Hit Points

--- app.rb
+++ app.rb
@@ -1,8 +1,9 @@
 require 'sinatra/base'
 
-
 class Battle < Sinatra::Base
+
   enable :sessions
+  MAX_HEALTH = 100
 
   get '/' do
     erb(:index)
@@ -16,8 +17,8 @@ class Battle < Sinatra::Base
   end
 
   get '/play' do
-    @player_1_name = session[:player_1_name]
-    @player_2_name = session[:player_2_name]
+    @player_1_name   = session[:player_1_name]
+    @player_2_name   = session[:player_2_name]
     erb(:play)
   end
 
--- spec/feature_tests/user_stories_spec.rb
+++ spec/feature_tests/user_stories_spec.rb
@@ -13,3 +13,17 @@ feature 'Enter names' do
     expect(page).to have_content 'Vicky vs Joe'
   end
 end
+
+# # USER STORY TWO
+# As Player 1,
+# So I can see how close I am to winning
+# I want to see Player 2's Hit Points
+feature 'Hit points' do
+  scenario "so player 1 knows how close they are to winning, player 1 can see player 2's hp" do
+    visit('/')
+    fill_in :player_1_name, with: 'Vicky'
+    fill_in :player_2_name, with: 'Joe'
+    click_button 'Submit'
+    expect(page).to have_content ' HP'
+  end
+end
