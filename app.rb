be86d7d3882ede4e49f7835c5490ecae55fbc84e
Vicky Gray <vickygray26@hotmail.com>
2016-02-04 12:14:08 +0000

    refactor

--- app.rb
+++ app.rb
@@ -24,18 +24,7 @@ class Battle < Sinatra::Base
     erb(:play)
   end
 
-  # post '/attack' do
-  #   @player_1 = $player_1
-  #   @player_2 = $player_2
-  #   @player_1_name = $player_1.name
-  #   @player_2_name = $player_2.name
-  #   @player_1.attack(@player_2)
-  #   @player_1_HP = $player_1.hit_points
-  #   @player_2_HP = $player_2.hit_points
-  #   redirect to('/attack_confirmation')
-  # end
-
-  post '/attack_confirmation' do
+  get '/attack_confirmation' do
     @player_1 = $player_1
     @player_2 = $player_2
     @player_1_name = $player_1.name
